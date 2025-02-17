359984d7408a478d00cf73346ebc8d565bc7731c
NITRO\arsen <arseniyvoytehovskiy@gmail.com>
2025-02-18 02:44:50 +0300

    refactoring: better Dependency Injection

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,14 @@
 using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.ConfigureServices(builder.Configuration);
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5000);
-});
-
-builder.WebHost.UseUrls("http://localhost:5000");
+builder.Services
+    .AddApplicationLayer()
+    .AddSwagger()
+    .AddMongoDb(builder.Configuration)
+    .ConfigureCors(builder.Configuration);
 
 var app = builder.Build();
+
 app.ConfigureMiddleware();
 app.Run();
